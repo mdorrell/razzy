0be69cf5d3bb59332e98b5194bdaa5b74c68a84b
Mike Dorrell <mike.dorrell@voxox.com>
2018-08-12 14:07:35 -0700

    make shutdown response actually shutdown

--- razzy/razzy.py
+++ razzy/razzy.py
@@ -5,38 +5,38 @@ from senses import *
 from commands import CommandResponse
 
 class Razzy():
-    
+
   currentState = ''
   logger = ""
   commands = ""
-  
+
   def getBrain(self):
     return self.brain
-  
+
   def getEars(self):
     return self.ears
-  
+
   def getMouth(self):
     return self.mouth
-  
+
   def getLights(self):
     return self.lights
-  
+
   def getWheels(self):
     return self.wheels
-  
+
   def getChat(self):
     return self.chat
 
   def getLogger(self):
-    return self.logger  
-  
+    return self.logger
+
   """
   Constructor
   """
   def __init__(self):
     self.initLogger()
-    
+
     # Get senses locally
     self.brain  = Brain(self.logger)
     self.ears   = Ears(self.logger)
@@ -44,7 +44,7 @@ class Razzy():
     self.lights = Lights(self.logger)
     self.wheels = Wheels(self.logger)
     self.chat   = Chat(self.logger)
-  
+
   """
   Initializes razzy
   """
@@ -66,16 +66,16 @@ class Razzy():
     out_hdlr.setLevel(logging.INFO)
     self.logger.addHandler(out_hdlr)
     self.logger.setLevel(logging.INFO)
-    
+
   """
   Main function while razzy is running
   """
   def run(self):
     # Get users command
     message = self.getMessage()
-    
+
     doContinue = True;
-    
+
     # Check if we must run a continue command
     self.doContinue(message)
 
@@ -84,26 +84,26 @@ class Razzy():
 
     # If we have a command, run it
     if (command):
-      
+
       # run the command
       self.processCommand(command, message)
 
     # If we don't have a command do default action
     else:
       self.doDefaultAction(message)
-    
+
     print "keep looping"
-    
+
     message = ''
-    
+
     return doContinue
-  
+
   """
   Get message from user
   :returns string message - What the user said
   """
   def getMessage(self):
-        
+
     # Turn on red light when listening
     self.getLights().redLight(1);
     self.getLights().blueLight(0);
@@ -115,42 +115,42 @@ class Razzy():
     message      = self.getEars().listen(listenTime)
 
     print message
-    
+
     return message
-  
+
   """
   If message was empty
   and current state has a continue command process it
   :param string message - What the user said
   """
   def doContinue(self, message):
-        
+
     # if message was empty, see if current state has a continue
     if (message == "" and self.currentState !='listen'):
       currentState = self.getBrain().getCurrentState()
       commandClass = self.commands[currentState]
       commandClass().doContinue(message)
-    
+
   """
   Get command from message if we have one
   :returns CommandBase command - Command class we will execute
   """
   def getCommand(self, message):
-        
+
     # Blue light when processing command
     self.getLights().redLight(0);
     self.getLights().blueLight(1);
 
     print "You said '" + message + "'"
     command = self.getBrain().checkMessage(message);
-    
+
     if (command):
       print "Command is " + command
     else:
       print "Command not recognized"
-      
+
     return command
-    
+
   """
   Run the given command
   :param CommandBase command - Command we will run
@@ -163,16 +163,18 @@ class Razzy():
     # Look for class named "command" in the list of commands
     commandClass = self.commands[command]
     response = commandClass().run(message, self)
-    
+
     # code 200 is speak and continue
     if (response.code == CommandResponse.CODE_OK):
       self.getMouth().speak(response.message)
     # code 0 is speak and exit
     elif (response.code == CommandResponse.CODE_EXIT):
       self.getMouth().speak(response.message)
-      
+      self.shutdown()
+      exit()
+
     print "done command"
-    
+
   """
   Do what we do when we don't have a command for the message
   :param string message - What the user said
@@ -181,7 +183,7 @@ class Razzy():
     #reply = chat.reply(message)
     #mouth.speak([reply])
     self.getMouth().speak([message])
-      
+
   """
   Shutdown razzy properly
   """
