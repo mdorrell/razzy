5dda6d36ccd2162a630fccda08c97849e85aef9a
Mike Dorrell <mike.dorrell@voxox.com>
2019-04-19 11:49:00 -0700

    removed chatterbot

--- razzy/senses/chat.py
+++ razzy/senses/chat.py
@@ -1,5 +1,5 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ChatterBotCorpusTrainer
 
 class Chat():
   #stores the chatterbot class
@@ -15,14 +15,15 @@ class Chat():
   Initialize chatbot
   """
   def init(self):
-    self.chatterbot = ChatBot("Razzy")
-    self.chatterbot.set_trainer(ChatterBotCorpusTrainer)
+    # self.chatterbot = ChatBot("Razzy")
+    # self.chatterbot.set_trainer(ChatterBotCorpusTrainer)
 
-    self.chatterbot.train("chatterbot.corpus.english")
+    # self.chatterbot.train("chatterbot.corpus.english")
     
   def reply(self, message):
     # Get a response to the input text 'How are you?'
-    response = self.chatterbot.get_response(message)
+    #response = self.chatterbot.get_response(message)
+    response = "I don't feel chatty today"
 
     print("{}".format(response))
     return str(response)
