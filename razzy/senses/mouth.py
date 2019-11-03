9a2c2c98f6b947ff856c2cec4eabe5f495af06f5
Mike Dorrell <mdorrell@gmail.com>
2019-11-03 10:24:45 -0800

    fixed speech slowdown bug

--- razzy/senses/mouth.py
+++ razzy/senses/mouth.py
@@ -10,9 +10,9 @@ class Mouth():
     self.logger = logger
     
   def speak(self, lines):
-    tts_engine = 'espeak -s130 '
+    tts_engine = 'espeak -s150 --stdout '
     for line in lines:
       self.logger.info(line)
-      os.system(tts_engine + ' "' + line + '"')
+      os.system(tts_engine + ' "' + line + '" | aplay')
       time.sleep(.5)
     return
