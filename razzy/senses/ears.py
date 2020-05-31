50a19280ee95f37489f9a42ef44e87ed928bec9a
Mike Dorrell <mdorrell@gmail.com>
2020-05-30 20:28:54 -0700

    add comments

--- razzy/senses/ears.py
+++ razzy/senses/ears.py
@@ -13,15 +13,20 @@ class Ears():
   """
   def __init__(self, logger, brain):
     self.logger = logger
-    self.recognizer = sr.Recognizer()
     self.brain = brain
 
-    self.recognizer.pause_threshold = 0.25  # seconds of non-speaking audio before a phrase is considered complete
-    self.recognizer.phrase_threshold = 0.5  # minimum seconds of speaking audio before we consider the speaking audio a phrase
-    self.recognizer.non_speaking_duration = 0.25  # seconds of non-speaking audio to keep on both sides of the recording
-    self.recognizer.energy_threshold = 1000  # Higher number makes the mic less sensitive
+    # Setup recognizer
+    self.recognizer = sr.Recognizer()
+    self.recognizer.pause_threshold          = 0.25  # seconds of non-speaking audio before a phrase is considered complete
+    self.recognizer.phrase_threshold         = 0.5  # minimum seconds of speaking audio before we consider the speaking audio a phrase
+    self.recognizer.non_speaking_duration    = 0.25  # seconds of non-speaking audio to keep on both sides of the recording
+    self.recognizer.energy_threshold         = 1000  # Higher number makes the mic less sensitive
     self.recognizer.dynamic_energy_threshold = False
 
+    # setup microphone
+    self.mic = sr.Microphone()
+    with self.mic as source:
+        self.recognizer.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
 
   """
   " stopListening
@@ -35,9 +40,6 @@ class Ears():
   " listenInBackground
   """
   def listenInBackground(self):
-    self.mic = sr.Microphone()
-    with self.mic as source:
-        self.recognizer.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
     self.stop_listening = self.recognizer.listen_in_background(self.mic, self.listenInBackgroundCallback)
 
   """
@@ -53,7 +55,6 @@ class Ears():
         if (message):
             self.brain.setMemory('ears', message)
 
-        #message = recognizer.recognize(audio)
         print("I heard " + message)  # received audio data, now need to recognize it
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
